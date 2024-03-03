f6afbff2d68d6ed3789ed8ebcf4bc4b7776e5afe
SUNIL PANDEY <97494052+rst9454@users.noreply.github.com>
2024-03-03 22:20:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ async function signInToYouTube() {
         await driver.get('https://www.c-sharpcorner.com/');
 
         await driver.findElement(By.id('loginBtn')).click();
-        await driver.findElement(By.id('TextBoxUserId')).sendKeys('demosunil9454@gmail.com', Key.RETURN);
-        await driver.findElement(By.id('TextBoxPassword')).sendKeys('Press@2021', Key.RETURN);
+        await driver.findElement(By.id('TextBoxUserId')).sendKeys('', Key.RETURN);
+        await driver.findElement(By.id('TextBoxPassword')).sendKeys('', Key.RETURN);
         await driver.findElement(By.id('ctl00_ContentMain_ButtonSignIN')).click();
         //Refresh the browser
         await driver.navigate().refresh();
@@ -51,3 +51,88 @@ async function signInToYouTube() {
 
 // Run the function
 signInToYouTube();
+
+
+
+---------------------------------------++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+In ASP.NET MVC, setting the Content Security Policy (CSP) header can help enhance the security of your web application by specifying the sources from which various resources can be loaded, thus mitigating the risks of cross-site scripting (XSS) attacks, data injection, and other web vulnerabilities.
+
+To set the Content Security Policy header in ASP.NET MVC, you can do this in the Global.asax.cs file or by using a middleware if you're using ASP.NET Core.
+
+Here's how you can set the CSP header in ASP.NET MVC in the Global.asax.cs file:
+
+Open your Global.asax.cs file.
+
+In the Application_Start method, add the following code to set the CSP header:
+
+protected void Application_Start()
+{
+    // Other application startup code
+
+    // Set Content Security Policy header
+    System.Web.Helpers.AntiForgeryConfig.SuppressXFrameOptionsHeader = true; // To allow framing if required
+    System.Web.Helpers.AntiForgeryConfig.SuppressContentSecurityPolicyHeader = true; // To suppress CSP header set by MVC
+
+    // Define your Content Security Policy
+    var policy = "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com"; // Example policy
+
+    // Set the Content-Security-Policy header
+    System.Web.Helpers.AntiForgeryConfig.AdditionalResponseHeaders["Content-Security-Policy"] = policy;
+}
+
+
+
+_++++++++++++++++++++++++++++++++++++++++++++++
+# Start a new feature
+git checkout -b new-feature main
+# Edit some files
+git add <file>
+git commit -m "Start a feature"
+# Edit some files
+git add <file>
+git commit -m "Finish a feature"
+# Merge in the new-feature branch
+git checkout main
+git merge new-feature
+git branch -d new-featureo
+_+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    PM> get-help entityframework
+                     _/\__
+               ---==/    \\
+         ___  ___   |.    \|\
+        | __|| __|  |  )   \\\
+        | _| | _|   \_/ |  //|\\
+        |___||_|       /   \\\/\\
+
+TOPIC
+    about_EntityFrameworkCore
+
+SHORT DESCRIPTION
+    Provides information about Entity Framework Core commands.
+
+LONG DESCRIPTION
+    This topic describes the Entity Framework Core commands. See https://docs.efproject.net for information on Entity Framework Core.
+
+    The following Entity Framework cmdlets are included.
+
+        Cmdlet                      Description
+        --------------------------  ---------------------------------------------------
+        Add-Migration               Adds a new migration.
+
+        Remove-Migration            Removes the last migration.
+
+        Scaffold-DbContext          Scaffolds a DbContext and entity type classes for a specified database.
+
+        Script-Migration            Generates a SQL script from migrations.
+
+        Update-Database             Updates the database to a specified migration.
+
+        Use-DbContext               Sets the default DbContext to use.
+
+SEE ALSO
+    Add-Migration
+    Remove-Migration
+    Scaffold-DbContext
+    Script-Migration
+    Update-Database
+    Use-DbContext
